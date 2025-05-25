19262af8a5398985c8563cf8a06e7bb8061dab3e
Geraldo0000 <124789861+Geraldo0000@users.noreply.github.com>
2025-05-25 18:34:25 -0300

    Move games on list

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -5,6 +5,7 @@ import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projection.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +20,35 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
-
+    // tenho que injetar aqui
+    @Autowired
+    private GameRepository gameRepository;
 
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
 
+    //movimentando o game na lista
+    @Transactional // garantir que serão executadas todas
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        // busca os games na lista
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        // agora vou remover o game de uma posição de origem
+        GameMinProjection obj = list.remove(sourceIndex);
+
+        // agora vou inserir o objeto na posição de destino
+        list.add(destinationIndex, obj);
+
+        // agora criar uma variavel para pegar entre o min e o max da origem e destino
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
 
     }
 }
